837b32aa6ee08289df7038280455d4a08f01eadc
manav <mnvgpl@gmail.com>
2024-07-14 17:19:36 +0530

    Initial commit

---
+++ src/components/GridComponent.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import "./styles/GridComponents.scss";
+import getNextAliveCells from "./getNextAliveCells"; // Import the algo function
+import { isEqual } from "lodash-es";
+import { initialActiveCells, toggleCellState } from "../utils/utils";
+
+const GridComponent = () => {
+  const gridSize = 30;
+
+  const InitialAliveCells = initialActiveCells();
+  const [aliveCells, setAliveCells] =
+    useState<Record<string, true>>(InitialAliveCells);
+  const [start, setStart] = useState<boolean>(false);
+
+  //For start method and getting the next cell
+  useEffect(() => {
+    if (start) {
+      var timeout = setTimeout(() => {
+        let updatedActiveCells = getNextAliveCells(aliveCells);
+
+        if (isEqual(updatedActiveCells, aliveCells)) {
+          setStart(!start);
+          return;
+        }
+        setAliveCells(updatedActiveCells);
+      }, 500);
+    }
+    return () => clearTimeout(timeout);
+  }, [aliveCells, start]);
+
+  // For onClick Event on the grid divs 
+  const toggleCell = (x: number, y: number) => {
+    const updatedActiveCells = toggleCellState(aliveCells, x, y, gridSize);
+    setAliveCells(updatedActiveCells);
+  };
+
+  //  For generating the Grid.....
+  const generateCells = () => {
+    const cells = [];
+    for (let i = 0; i < gridSize; i++) {
+      for (let j = 0; j < gridSize; j++) {
+        const cellKey: string = `${i}-${j}`;
+        cells.push(
+          <div
+            key={cellKey}
+            id={cellKey}
+            className={`grid-item ${aliveCells[cellKey] ? "alive" : "died"}`}
+            onClick={() => toggleCell(i, j)}
+          ></div>
+        );
+      }
+    }
+    return cells;
+  };
+
+  const cells = generateCells();
+  const handleReset = () => {
+    setAliveCells(InitialAliveCells);
+    setStart(false);
+  };
+  const handleClear = () => {
+    setAliveCells({});
+    setStart(false);
+  };
+  const handleStartStop = () => {
+    setStart(!start);
+  };
+
+  return (
+    <div className="main-container">
+      <div className="description">
+        <h1>Conway's <br/><span className="highlight">Game Of Life</span></h1>
+      </div>
+      <div className="grid-container">{cells}</div>
+      <div className="action-container">
+        <button className="start" onClick={handleStartStop}>
+          {!start ? "Start" : "Stop"}
+        </button>
+        <button className="reset" onClick={handleReset}>
+          Reset
+        </button>
+        <button className="clear" onClick={handleClear}>
+          Clear All
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default GridComponent;
---
+++ src/components/getNextAliveCells.tsx
@@ -0,0 +1,65 @@
+const getNextAliveCells = (aliveCells: Record<string, true>) => {
+  let map = new Map<string, number>(); // Tracks the number of alive neighbours for each cell
+  let checkedCells = new Map(); // Tracks cells that have already been processed
+
+  function mapSet(x: number, y: number, subX: number, subY: number) {
+    const currentCellKey = `${x}-${y}`;
+    const neighbourCellKey = `${x + subX}-${y + subY}`;
+
+    //Check if its out of boundry
+    if (x < 0 || y < 0 || x > 30 || y > 30) {
+      return;
+    }
+
+    if (!checkedCells.has(neighbourCellKey)) {
+      const currCellCount = map.get(currentCellKey) || 0;
+      const neighbourCellCount = map.get(neighbourCellKey) || 0;
+
+      // if the neighbour is alive
+      if (aliveCells[neighbourCellKey]) {
+        // updating main element.....
+        map.set(currentCellKey, currCellCount + 1);
+      }
+
+      // else just update the neighbours ....
+      map.set(neighbourCellKey, neighbourCellCount + 1);
+    }
+  }
+
+  // Iterate over each alive cell
+  Object.keys(aliveCells).forEach((key) => {
+    let [x, y] = key.split("-").map(Number); // Split the key to get x and y coordinates
+
+    // Check all 8 neighbours of the current cell (starting from top-left, going clockwise)
+    mapSet(x, y, -1, -1); // Top-left neighbour
+    mapSet(x, y, -1, 0); // Top neighbour
+    mapSet(x, y, -1, 1); // Top-right neighbour
+    mapSet(x, y, 0, -1); // Left neighbour
+    mapSet(x, y, 0, 1); // Right neighbour
+    mapSet(x, y, 1, -1); // Bottom-left neighbour
+    mapSet(x, y, 1, 0); // Bottom neighbour
+    mapSet(x, y, 1, 1); // Bottom-right neighbour
+
+    checkedCells.set(`${x}-${y}`, true); // Mark the current cell as checked
+  });
+
+  // Here we Apply the rules of game of life
+  const currentAlive: Record<string, true> = {};
+  const entries = Array.from(map.entries());
+  for (const [key, value] of entries) {
+    // If a cell has exactly 3 neighbours, it becomes alive
+    if (value === 3) {
+      currentAlive[key] = true;
+    }
+    // If a cell has exactly 2 neighbours and was previously alive, it remains alive
+    else if (value === 2 && checkedCells.has(key)) {
+      currentAlive[key] = true;
+    }
+    // Other cells will die anyways so we dont need any condition for them
+  }
+  // console.log(currentAlive);
+
+  return currentAlive;
+};
+
+export default getNextAliveCells;
